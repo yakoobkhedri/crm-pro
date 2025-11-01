aca6622309a4cd024fddfe8cb41b0a30b5c029a9
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-11-02 00:54:32 +0330

    first commit

---
+++ js/select2.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.js-example-basic-single').select2({
+    tags: true
+  });
+});
+$(document).ready(function() {
+  $('.js-example-basic-multiple').select2({
+    tags: true
+  });
+});
